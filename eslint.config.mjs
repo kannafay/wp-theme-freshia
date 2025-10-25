589ca2f2ca815896e7d1e89582d59996f8255c56
神秘布偶猫 <kannafay@gmail.com>
2025-10-26 01:08:15 +0800

    1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
 	{
-		files: ['**/*.{js,mjs,cjs,vue}'],
+		files: ['**/*.{js,mjs,cjs,jsx,vue}'],
 		plugins: { js },
 		extends: ['js/recommended'],
 		languageOptions: { globals: globals.browser },
--- src/main.js
+++ src/main.js
@@ -15,71 +15,71 @@ Alpine.start()
 
 // SwupProgressPlugin
 const swupProgressPlugin = new SwupProgressPlugin({
-    className: 'swup-progress-bar',
-    transition: 300,
-    delay: 300,
+	className: 'swup-progress-bar',
+	transition: 300,
+	delay: 300,
 })
 // SwupPreloadPlugin
 const swupPreloadPlugin = new SwupPreloadPlugin()
 // SwupHeadPlugin
 const swupHeadPlugin = new SwupHeadPlugin({
-    persistTags: [
-        'style[data-vite-dev-id]',
-        'link[rel="stylesheet"][data-vite-dev-id]',
-    ],
+	persistTags: [
+		'style[data-vite-dev-id]',
+		'link[rel="stylesheet"][data-vite-dev-id]',
+	],
 })
 // SwupScriptsPlugin
 const swupScriptsPlugin = new SwupScriptsPlugin({
-    head: false,
-    body: true,
+	head: false,
+	body: true,
 })
 // Swup 实例
 const swup = new Swup({
-    animationSelector: false,
-    linkSelector: 'a[href]:not([target="_blank"])',
-    linkToSelf: 'navigate',
-    hooks: {
-        'link:click': () => {
-            // console.log('link:click')
-        },
-        'visit:start': () => {
-            // console.log('visit:start')
-        },
-        'content:replace': () => {
-            // console.log('content:replace')
-            init()
-        },
-        'page:view': () => {
-            // console.log('page:view')
-        },
-    },
-    plugins: [
-        swupProgressPlugin,
-        swupPreloadPlugin,
-        swupHeadPlugin,
-        swupScriptsPlugin,
-    ],
+	animationSelector: false,
+	linkSelector: 'a[href]:not([target="_blank"])',
+	linkToSelf: 'navigate',
+	hooks: {
+		'link:click': () => {
+			// console.log('link:click')
+		},
+		'visit:start': () => {
+			// console.log('visit:start')
+		},
+		'content:replace': () => {
+			// console.log('content:replace')
+			init()
+		},
+		'page:view': () => {
+			// console.log('page:view')
+		},
+	},
+	plugins: [
+		swupProgressPlugin,
+		swupPreloadPlugin,
+		swupHeadPlugin,
+		swupScriptsPlugin,
+	],
 })
 
 // 初始化
 document.addEventListener('DOMContentLoaded', () => {
-    init()
-    // scrollReveal()
+	init()
+	// scrollReveal()
 })
 
 // 出场动画
 swup.hooks.replace('animation:out:await', async () => {
-    await gsap.to('main', {
-        opacity: 0,
-        duration: 0.2,
-    })
+	await gsap.to('main', {
+		opacity: 0,
+		duration: 0.2,
+	})
 })
 
 // 入场动画
 swup.hooks.replace('animation:in:await', async () => {
-    // scrollReveal()
-    await gsap.from('main', {
-        opacity: 0,
-        duration: 0.2,
-    })
+	// scrollReveal()
+	await gsap.from('main', {
+		opacity: 0,
+		duration: 0.2,
+	})
 })
